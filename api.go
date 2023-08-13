61cc46e2305523e4c3d41f4f7693457bf6f099ce
ringsaturn <ringsaturn.me@gmail.com>
2023-08-13 16:50:31 +0800

    Reduce deps (#9)

--- api.go
+++ api.go
@@ -4,10 +4,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"time"
 
-	"github.com/ringsaturn/requests"
 	"go.uber.org/ratelimit"
 )
 
@@ -18,12 +19,26 @@ type Client struct {
 	bulkRL     ratelimit.Limiter
 }
 
-func NewClient(apiKey string) (*Client, error) {
+type ClientOption func(*Client)
+
+func WithHTTPClient(httpClient *http.Client) ClientOption {
+	return func(c *Client) {
+		c.httpClient = httpClient
+	}
+}
+
+func NewClient(apiKey string, opts ...ClientOption) (*Client, error) {
 	client := &Client{
 		apiKey:     apiKey,
 		singleRL:   ratelimit.New(1000, ratelimit.Per(60*time.Second)),
 		bulkRL:     ratelimit.New(100, ratelimit.Per(60*time.Second)),
-		httpClient: http.DefaultClient,
+		httpClient: nil,
+	}
+	for _, opt := range opts {
+		opt(client)
+	}
+	if client.httpClient == nil {
+		client.httpClient = http.DefaultClient
 	}
 	return client, nil
 }
@@ -44,6 +59,34 @@ type getPlacekeyFromGeoRequest struct {
 	Query getPlacekeyFromGeoRequestParam `json:"query"`
 }
 
+func (c *Client) req(ctx context.Context, method string, url string, body io.Reader) (*Response, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, "https://api.placekey.io/v1/placekey/", body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("apiKey", c.apiKey)
+	req.Header.Set("Content-Type", "application/json")
+	httpResp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer httpResp.Body.Close()
+
+	respBody, err := io.ReadAll(httpResp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if httpResp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("http status code: %d, body: %s", httpResp.StatusCode, string(respBody))
+	}
+	resp := &Response{}
+	err = json.Unmarshal(respBody, resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
 // GetPlacekeyFromGeo almost equal like GeoToPlacekey, no `what` part.
 func (c *Client) GetPlacekeyFromGeo(
 	ctx context.Context, latitude float64, longitude float64,
@@ -59,21 +102,7 @@ func (c *Client) GetPlacekeyFromGeo(
 	if err != nil {
 		return nil, err
 	}
-
-	resp := &Response{}
-	err = requests.ReqWithExpectJSONResponse(
-		ctx, c.httpClient,
-		"POST", "https://api.placekey.io/v1/placekey/", bytes.NewBuffer(apiBody),
-		resp,
-		requests.HeaderOption(map[string]string{
-			"apiKey":       c.apiKey,
-			"Content-Type": "application/json",
-		}),
-	)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
+	return c.req(ctx, http.MethodPost, "https://api.placekey.io/v1/placekey/", bytes.NewBuffer(apiBody))
 }
 
 type getPlacekeyFromAddressRequestParam struct {
@@ -111,19 +140,5 @@ func (c *Client) GetPlacekeyFromAddress(
 	if err != nil {
 		return nil, err
 	}
-
-	resp := &Response{}
-	err = requests.ReqWithExpectJSONResponse(
-		ctx, c.httpClient,
-		"POST", "https://api.placekey.io/v1/placekey/", bytes.NewBuffer(apiBody),
-		resp,
-		requests.HeaderOption(map[string]string{
-			"apiKey":       c.apiKey,
-			"Content-Type": "application/json",
-		}),
-	)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
+	return c.req(ctx, http.MethodPost, "https://api.placekey.io/v1/placekey/", bytes.NewBuffer(apiBody))
 }
